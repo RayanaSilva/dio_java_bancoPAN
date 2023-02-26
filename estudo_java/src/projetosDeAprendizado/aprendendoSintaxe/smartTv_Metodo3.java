0b44ceb61b3d9d2756cd794eed11bc21c5cc7260
RayanaSilva <rayana.silvaa@outlook.com>
2023-02-26 00:48:42 -0300

    Iniciando projeto SmartTV do método 3

--- estudo_java/src/projetosDeAprendizado/aprendendoSintaxe/smartTv_Metodo3.java
+++ estudo_java/src/projetosDeAprendizado/aprendendoSintaxe/smartTv_Metodo3.java
@@ -1,5 +1,14 @@
 package projetosDeAprendizado.aprendendoSintaxe;
 
 public class smartTv_Metodo3 {
-    
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 15;
+
+    public void ligar(){
+        ligada=true;
+    }
+    public void desligar(){
+        ligada=false;
+    }
 }
--- estudo_java/src/projetosDeAprendizado/aprendendoSintaxe/usuario_Metodo3.java
+++ estudo_java/src/projetosDeAprendizado/aprendendoSintaxe/usuario_Metodo3.java
@@ -2,4 +2,11 @@ package projetosDeAprendizado.aprendendoSintaxe;
 
 public class usuario_Metodo3 {
     
+    smartTv_Metodo3 smartTv_Metodo3 = new smartTv_Metodo3();
+   
+
+    System.out.println("Status da TV: " + smartTv_Metodo3.ligada);
+
+  
+
 }
