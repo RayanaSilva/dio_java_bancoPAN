a1f282560090738b9d6f58df16737c784362f1ab
RayanaSilva <rayana.silvaa@outlook.com>
2023-03-06 10:34:17 -0300

    Testando ações do usuário utilizando a Smart TV.

--- estudo_java/src/projetosDeAprendizado/aprendendoSintaxe/smartTv_Metodo3.java
+++ estudo_java/src/projetosDeAprendizado/aprendendoSintaxe/smartTv_Metodo3.java
@@ -11,4 +11,24 @@ public class smartTv_Metodo3 {
     public void desligar(){
         ligada=false;
     }
+
+    public void aumentarVolume(){
+        volume++;
+    }
+
+    public void diminuirVolume(){
+        volume--;
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal (){
+        canal--;
+    }
 }
--- estudo_java/src/projetosDeAprendizado/aprendendoSintaxe/usuario_Metodo3.java
+++ estudo_java/src/projetosDeAprendizado/aprendendoSintaxe/usuario_Metodo3.java
@@ -1,12 +1,32 @@
 package projetosDeAprendizado.aprendendoSintaxe;
 
 public class usuario_Metodo3 {
-    
-    smartTv_Metodo3 smartTv_Metodo3 = new smartTv_Metodo3();
+    public static void main(String[] args) throws Exception {
+        
+        smartTv_Metodo3 smartTv_Metodo3 = new smartTv_Metodo3();
    
+        System.out.println("TV ligada? " + smartTv_Metodo3.ligada);
+        System.out.println("Canal: " + smartTv_Metodo3.canal);
+        System.out.println("Volume: " + smartTv_Metodo3.volume);
 
-    System.out.println("Status da TV: " + smartTv_Metodo3.ligada);
+        smartTv_Metodo3.ligar();
+        System.out.println("\nTV foi ligada? " + smartTv_Metodo3.ligada);
+        smartTv_Metodo3.diminuirVolume();
+        smartTv_Metodo3.diminuirVolume();
+        smartTv_Metodo3.diminuirVolume();
+        System.out.println("\nVolume: " + smartTv_Metodo3.volume);
 
-  
+        smartTv_Metodo3.mudarCanal(8);
+        System.out.println("\nCanal: " + smartTv_Metodo3.canal);
+        smartTv_Metodo3.aumentarCanal();
+        smartTv_Metodo3.aumentarCanal();
+        smartTv_Metodo3.aumentarCanal();
+        smartTv_Metodo3.aumentarCanal();
 
+        System.out.println("\nTV ligada? " + smartTv_Metodo3.ligada);
+        System.out.println("Canal: " + smartTv_Metodo3.canal);
+        System.out.println("Volume: " + smartTv_Metodo3.volume);
+
+    }
+    
 }
