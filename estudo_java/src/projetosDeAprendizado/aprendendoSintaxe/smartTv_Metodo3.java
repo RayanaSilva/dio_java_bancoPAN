2fd721f6e53b79d566552817109263b9bae90af0
RayanaSilva <rayana.silvaa@outlook.com>
2023-03-06 10:47:05 -0300

    Comentário para compreender como funciona o Escopo

--- estudo_java/src/projetosDeAprendizado/aprendendoSintaxe/smartTv_Metodo3.java
+++ estudo_java/src/projetosDeAprendizado/aprendendoSintaxe/smartTv_Metodo3.java
@@ -1,6 +1,14 @@
 package projetosDeAprendizado.aprendendoSintaxe;
 
 public class smartTv_Metodo3 {
+
+    /* Aprendendo sobre escopo.
+     * As variáveis ligada, canal e volume são de escopo class, ou seja, está disponível
+     * em todo nosso código da smartTv_Metodo3.
+     * Já a variável novoCanal é variável de escopo local, ou seja, está disponível 
+     * somente em mudarCanal.
+     */
+
     boolean ligada = false;
     int canal = 1;
     int volume = 15;
