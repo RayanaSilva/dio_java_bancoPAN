b8852520b884e44a60d7d64650ceac3ccc5c7fa2
RayanaSilva <rayana.silvaa@outlook.com>
2023-03-08 14:36:45 -0300

    Exercitando o aprendizado.

---
+++ estudo_java/src/exercicios/MaiorEMedia.java
@@ -0,0 +1,34 @@
+package exercicios;
+
+import java.util.Scanner;
+
+/* Maior e Média: Faça um programa que leia 5 números e informe 
+ * o maior número e a média desses números. 
+ */
+
+public class MaiorEMedia {
+    public static void main(String[] args){
+
+        int numero;    
+        int i = 0;   
+        int maior = 0;
+        int soma = 0;
+
+        Scanner ler = new Scanner(System.in);
+
+        do {
+            System.out.println("Digite um número: ");
+            numero = ler.nextInt();
+
+            soma += numero;
+
+            if(numero>maior) maior = numero;
+            
+            i++;
+
+        } while (i<5);
+
+        System.out.println("\nO maior número digitado foi: " + maior);
+        System.out.println("A média de todos números digitados foi: " + (soma/5));
+    }
+}
